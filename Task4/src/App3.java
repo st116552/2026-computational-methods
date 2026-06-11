f400e5273883cd1c50796cc5e84eb637ff51f5d0
st116552 <st116552@student.spbu.ru>
2026-06-11 15:43:05 +0300

    Update App3.java

--- Task4/src/App3.java
+++ Task4/src/App3.java
@@ -29,9 +29,9 @@ public class App3 {
             // 3. Выбор функции
             System.out.println("\nВыберите функцию f(x) для интегрирования:");
             System.out.println("0: f0(x) = 1.0");
-            System.out.println("1: f1(x) = x");
-            System.out.println("2: f2(x) = x^2");
-            System.out.println("3: f3(x) = x^3");
+            System.out.println("1: f1(x) = x + 1");
+            System.out.println("2: f2(x) = x^2 + 2x + 2");
+            System.out.println("3: f3(x) = x^3 + 3 x^2 + 3x + 3");
             System.out.println("4: f4(x) = sin(x)");
             System.out.print("Введите номер функции (0-4): ");
             int funcNumber = sc.nextInt();
@@ -43,8 +43,7 @@ public class App3 {
             double h = (b - a) / m;
             double J = integral(funcNumber, a, b);
 
-            System.out.println("\nПараметры задачи");
-            System.out.printf("A = %.4f, B = %.4f, m = %d, h = %.6e\n", a, b, m, h);
+            System.out.printf("h = %.10f\n",h);
             System.out.printf("Точное значение интеграла J = %.10f\n", J);
 
             // Вычисление J(h) для всех 5 методов
@@ -60,7 +59,7 @@ public class App3 {
             for (int i = 0; i < 5; i++) {
                 double absErr = Math.abs(J - jh[i]);
                 double relErr = (Math.abs(J) > 1e-15) ? (absErr / Math.abs(J)) * 100 : 0.0;
-                System.out.printf("%-18s: J(h) = %-15.10f  Абс. погр. = %-10.2e  Отн. погр. = %.2e%%\n", 
+                System.out.printf("%-18s: J(h) = %.15f  Абс. погр. = %.15f  Отн. погр. = %.15f %%\n", 
                                             names[i], jh[i], absErr, relErr);
             }
 
@@ -77,7 +76,6 @@ public class App3 {
                 }
 
                 int mNew = m * l;
-                double hNew = (b - a) / mNew;
                 
                 // Вычисление J(h/l)
                 double[] jhl = new double[5];
@@ -87,7 +85,7 @@ public class App3 {
                 jhl[3] = quadTrap(funcNumber, a, b, mNew);
                 jhl[4] = quadSimp(funcNumber, a, b, mNew);
 
-                // Порядок точности методов (p): Лев/Прав=1, Сред/Трап=2, Симпсон=4
+                // Порядок точности методов p: Лев/Прав=1, Сред/Трап=2, Симпсон=4
                 int[] p = {1, 1, 2, 2, 4};
                 double[] jp = new double[5]; // Уточненные значения
 
@@ -96,8 +94,8 @@ public class App3 {
                     jp[i] = jhl[i] + (jhl[i] - jh[i]) / (Math.pow(l, p[i]) - 1);
                 }
 
-                // Вывод таблицы результатов уточнения
-                System.out.printf("  %-18s   %-12s   %-12s   %-12s   %-12s   %-12s   %-12s  \n", 
+                // Вывод таблицы результатов уточнения0
+                System.out.printf("  %-20s   %-18s   %-18s   %-18s   %-18s   %-18s   %-18s  \n", 
                         "Метод (СКФ)", "J(h)", "|J - J(h)|", "J(h/l)", "|J - J(h/l)|", "Jp", "|J - Jp|");
                 
                 for (int i = 0; i < 5; i++) {
@@ -105,7 +103,7 @@ public class App3 {
                     double errHl = Math.abs(J - jhl[i]);
                     double errP = Math.abs(J - jp[i]);
                     
-                    System.out.printf("  %-18s   %-12.6e   %-12.2e   %-12.6e   %-12.2e   %-12.6e   %-12.2e  \n",
+                    System.out.printf("  %-18s   %.15f   %.15f   %.15f   %.15f   %.15f   %.15f  \n",
                             names[i], jh[i], errH, jhl[i], errHl, jp[i], errP);
                 }
                 
@@ -126,9 +124,9 @@ public class App3 {
     private static double f(int idx, double x) {
         switch (idx) {
             case 0: return 1.0;
-            case 1: return x;
-            case 2: return x * x;
-            case 3: return x * x * x;
+            case 1: return x + 1;
+            case 2: return x * x + 2 * x + 2;
+            case 3: return x * x * x + 3 * x * x + 3 * x + 3;
             case 4: return Math.sin(x);
             default: return 0.0;
         }
@@ -140,9 +138,9 @@ public class App3 {
     private static double integral(int idx, double a, double b) {
         switch (idx) {
             case 0: return (b - a);
-            case 1: return (b * b - a * a) / 2.0;
-            case 2: return (b * b * b - a * a * a) / 3.0;
-            case 3: return (b * b * b * b - a * a * a * a) / 4.0;
+            case 1: return (b * b - a * a) / 2.0 + (b - a);
+            case 2: return (b * b * b - a * a * a) / 3.0 + 2 * (b * b - a * a) / 2.0 + 2 * (b - a);
+            case 3: return (b * b * b * b - a * a * a * a) / 4.0 + 3 *(b * b * b - a * a * a) / 3.0 + 3 * (b * b - a * a) / 2.0 + 3 * (b - a);
             case 4: return -Math.cos(b) + Math.cos(a);
             default: return 0.0;
         }
@@ -201,14 +199,10 @@ public class App3 {
      */
     private static double quadSimp(int idx, double a, double b, int m) {
         double h = (b - a) / m;
-        double sum = f(idx, a) + f(idx, b);
-        for (int i = 1; i < m; i++) {
-            if (i % 2 == 1) {
-                sum += 4.0 * f(idx, a + i * h);
-            } else {
-                sum += 2.0 * f(idx, a + i * h);
-            }
+        double sum = 0;
+        for (int i = 1; i <= m; i++) {
+            sum += h / 6.0 * (f(idx, a + ((i - 1) * h)) + 4 * f(idx, a + ((2 * i - 1) / 2.0 * h)) + f(idx, a + (i * h)));
         }
-        return (h / 3.0) * sum;
+        return sum;
     }
 }
